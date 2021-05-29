40fb9fe369f7d1a8aba2a701539c8f1f853daf6c
krishpranav <krisna.pranav@gmail.com>
2021-05-29 16:35:08 +0530

    [+] Added Difference functions [+]

--- main.go
+++ main.go
@@ -824,3 +824,16 @@ func deleteUnusefulIgnoreresponses(input []string) []string {
 	result = Difference(input, toberemoved)
 	return result
 }
+
+func Difference(a, b []string) (diff []string) {
+	m := make(map[string]bool)
+	for _, item := range b {
+		m[item] = true
+	}
+	for _, item := range a {
+		if _, ok := m[item]; !ok {
+			diff = append(diff, item)
+		}
+	}
+	return
+}
