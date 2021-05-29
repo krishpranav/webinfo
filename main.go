61bdfcfd0618cc5ce04a9d83be71bb88a2e5d3a3
krishpranav <krisna.pranav@gmail.com>
2021-05-29 18:41:12 +0530

    [+] create output file function [+]

--- main.go
+++ main.go
@@ -1107,3 +1107,44 @@ func createOutputFolder() {
 		os.Exit(1)
 	}
 }
+
+func createOutputFile(target string, subcommand string, format string) string {
+	target = replaceBadCharacterOutput(target)
+	filename := "output-scilla" + "/" + target + "." + subcommand + "." + format
+	_, err := os.Stat(filename)
+
+	if os.IsNotExist(err) {
+		if _, err := os.Stat("output-scilla/"); os.IsNotExist(err) {
+			createOutputFolder()
+		}
+		f, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Println("Can't create output file.")
+			os.Exit(1)
+		}
+		f.Close()
+	} else {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("The output file already esists, do you want to overwrite? (Y/n): ")
+		text, _ := reader.ReadString('\n')
+		answer := strings.ToLower(text)
+		answer = strings.TrimSpace(answer)
+
+		if answer == "y" || answer == "yes" || answer == "" {
+			f, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				fmt.Println("Can't create output file.")
+				os.Exit(1)
+			}
+			err = f.Truncate(0)
+			if err != nil {
+				fmt.Println("Can't create output file.")
+				os.Exit(1)
+			}
+			f.Close()
+		} else {
+			os.Exit(1)
+		}
+	}
+	return filename
+}
